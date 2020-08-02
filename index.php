11086b206f6ebe75d4c526a6053f64532c82dc23
J-HaleOf76 <johnhale1976@gmail.com>
2020-08-02 12:47:49 -0400

    finished course

--- index.php
+++ index.php
@@ -14,33 +14,61 @@
     echo "<h2>You have $".$credit." in your wallet.</h2>";
 
 
-    $products['Computer']=750;
-    $products['Car']=15000;
-    $products['iPhone']=1000;
-    $products['Toaster']=75;
+//     $products['Computer']=750;
+//     $products['Car']=15000;
+//     $products['iPhone']=1000;
+//     $products['Toaster']=75;
 
-    foreach($products as $key => $value){
-	    echo "<p>The ".$key." costs ".$value."</p>";
-    }
-    echo "<h2>Items you can afford</h2>";
+//     $taxRate=0.0825;
 
-foreach($products as $key => $value){
-  if($value <= $credit ){
-  	echo "<p>".$key."</p>"; 
-  }
-}
-$amount=800;
+//     foreach($products as $key => $value){
+// 	    echo "<p>The ".$key." costs ".$value."</p>";
+//     }
+//     echo "<h2>Items you can afford</h2>";
+
+// foreach($products as $key => $value){
+//   if($value <= $credit ){
+//   	echo "<p>".$key."</p>"; 
+//   }
+// }
+// $amount=800;
+// $addedTax= $amount*$taxRate;  //amount = 800, tax = .0825
+// // echo $addedTax;
+
+
+// function tax_calc($amount,$tax){
+//     $calculate_tax = $amount*$tax;
+//     $amount = round($amount+$calculate_tax,2);
+//     return $amount;
+// }
+// echo tax_calc(750, 0.0223);
+
+$products['Computer']=750;
+$products['Car']=15000;
+$products['iPhone']=1000;
+$products['Toaster']=75;
+
 $taxRate=0.0825;
-$addedTax= $amount*$taxRate;  //amount = 800, tax = .0825
-// echo $addedTax;
-
 
 function tax_calc($amount,$tax){
-    $calculate_tax = $amount*$tax;
-    $amount = round($amount+$calculate_tax,2);
-    return $amount;
+    $addedTax = $amount*$tax;
+    $amountWithTax = round($amount+$addedTax,2);
+    return $amountWithTax;
 }
-echo tax_calc(750, 0.0223);
+
+foreach($products as $key => $value){
+    $costWithTax = tax_calc($value,$taxRate);
+    echo "<p>The ".$key." costs ".$costWithTax." with tax</p>";
+}
+
+echo "<h2>Items you can afford</h2>";
+
+foreach($products as $key => $value){
+    $costWithTax = tax_calc($value,$taxRate);
+    if($costWithTax <= $credit ){
+        echo "<p>".$key."</p>"; 
+    }
+}
 
 
     ?>
